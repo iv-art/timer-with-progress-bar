7b3030396e86ebce1469440482251b8412e370b4
Igor Osipov <ocinov@yandex.ru>
2022-12-22 20:59:11 +0500

    плющим с кнопкой, но не возвращается в начальное положение

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,44 @@
-window.addEventListener('load', function(){
-let startTime = 15;
+window.addEventListener('load', function () {
+	let buttonSqueeze = document.getElementById('startSqueeze');
 
-	let t2 = new TimerWithBar('.timer2', startTime, '.progressBar2');
-
-
-	let t3 = new TimerWithBar('.timer3', startTime, 'img');
-
-let t4 = new TimerWithBar('.timer4', startTime, '.t4-bar');
-let t5 = new TimerWithBar('.timer5', startTime, '.progressBar5');
-
+	document.querySelector('.start-time').addEventListener('input', filterDigits);
 	
+	function filterDigits () {
+		let regexPattern = /\d/;
+		let enteredString = this.value;
+		let newString = ''
+	  for (let i of enteredString) {
+		if (regexPattern.test(i)) {
+	newString += i
+		}
+	  }
+	  this.value = newString;
+	}
+
+	buttonSqueeze.addEventListener('click', function(){
+		let seconds = document.querySelector('.start-time').value;
+		if (seconds == 0 || seconds == '') {
+			document.querySelector('.out-1').innerHTML = 'Вы не ввели число!'
+		}
+		else {
+			let startTime = +seconds;
 	
-});
+			let t2 = new TimerWithBar('.timer2', startTime, '.progressBar2');
+		
+		
+			let t3 = new TimerWithBar('.timer3', startTime, 'img');
+		
+		let t4 = new TimerWithBar('.timer4', startTime, '.t4-bar');
+		let t5 = new TimerWithBar('.timer5', startTime, '.progressBar5');
+		}
+	
+		
+	});
+})
+
+
+
+
 
 class Timer{
 	constructor(boxSelector, initialTime){
@@ -41,6 +68,7 @@ class Timer{
 
 	stop(){
 		clearInterval(this.interval);
+
 	}
 }
 
@@ -67,19 +95,11 @@ class TimerWithBar extends TimerFormatted {
 	constructor(boxSelector, initialTime, barSelector) {
 		super(boxSelector, initialTime);
 		this.barElement = document.querySelector(barSelector);
+		this.barElementInitialWidth = this.barElement.clientWidth;
 		this.step = +(this.barElement.clientWidth / initialTime).toFixed(4);
 		this.widthReducedOnce = this.barElement.clientWidth;
 	}
- showBar = () => {
-	console.log(this.barElement);
-	console.log(this.barElement.offsetWidth);
- }
 
- reduceBar = () => {
-	let reducingBar = 0;
-	let barWidth = this.barElement.offsetWidth;
-
- }
 tick = () => {
 	super.tick();
 	
@@ -88,12 +108,15 @@ tick = () => {
 		this.widthReducedOnce = 0;
 	}
 	let currentHeight = this.barElement.offsetHeight;
-	
-	console.log(this.widthReducedOnce, this.step);
 	this.barElement.style.width = `${this.widthReducedOnce}px`
 	this.barElement.style.height = `${currentHeight}px`
 }
-
+stop = () => {
+	super.stop();
+	this.barElement.style.width = `${this.barElementInitialWidth}px`;
+	console.log(this.barElement.style.width);
+console.log(this.barElement);
+	}
  
 }
 
